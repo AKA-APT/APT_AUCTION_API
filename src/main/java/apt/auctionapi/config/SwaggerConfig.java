cc479f6b00ce4f7869e04c138f1322d92f07eb1f
강인화 <rkddlsghk1998@gmail.com>
2025-02-09 00:47:12 +0900

    feat: Swagger 서버 url 설정

--- src/main/java/apt/auctionapi/config/SwaggerConfig.java
+++ src/main/java/apt/auctionapi/config/SwaggerConfig.java
@@ -1,25 +1,33 @@
 package apt.auctionapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
+    private final String serverUrl;
+
+    public SwaggerConfig(
+        @Value("${swagger.server-url}") String serverUrl
+    ) {
+        this.serverUrl = serverUrl;
+    }
+
     @Bean
     public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl(serverUrl);
         return new OpenAPI()
             .info(new Info()
                 .title("Auction API")
                 .version("1.0")
-                .description("경매 데이터를 관리하는 API 문서")
-                .contact(new Contact()
-                    .name("Support Team")
-                    .email("support@example.com")
-                    .url("https://example.com")));
+                .description("경매 데이터를 관리하는 API 문서"))
+            .addServersItem(server);
     }
 }
