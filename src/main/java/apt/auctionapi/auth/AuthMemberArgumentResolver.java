1e3b1bb59e40054c82a78865aacf3e3541647e6e
강인화 <rkddlsghk1998@gmail.com>
2025-02-13 00:12:21 +0900

    feat: 리졸버가 Member 반환하도록 변경

--- src/main/java/apt/auctionapi/auth/AuthMemberArgumentResolver.java
+++ src/main/java/apt/auctionapi/auth/AuthMemberArgumentResolver.java
@@ -1,9 +1,5 @@
 package apt.auctionapi.auth;
 
-import apt.auctionapi.entity.Member;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -11,26 +7,36 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import apt.auctionapi.domain.SessionUser;
+import apt.auctionapi.entity.Member;
+import apt.auctionapi.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
 @Component
 @RequiredArgsConstructor
 public class AuthMemberArgumentResolver implements HandlerMethodArgumentResolver {
 
+    private final MemberRepository memberRepository;
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.hasParameterAnnotation(AuthMember.class) &&
-                parameter.getParameterType().equals(Member.class);
+            parameter.getParameterType().equals(Member.class);
     }
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
-                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = (HttpServletRequest)webRequest.getNativeRequest();
         HttpSession session = request.getSession(false);
 
         if (session == null) {
             return null;
         }
 
-        return session.getAttribute("user");
+        return memberRepository.findById(((SessionUser)session.getAttribute("user")).id())
+            .orElseThrow(() -> new RuntimeException("해당 사용자가 존재하지 않습니다."));
     }
 }
