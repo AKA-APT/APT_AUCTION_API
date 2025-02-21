41c2408d575fdd3282f50d03d213516cb1de725a
강인화 <rkddlsghk1998@gmail.com>
2025-02-21 16:57:19 +0900

    feat: 타입 컨버터 추가

--- src/main/java/apt/auctionapi/config/MongoConfig.java
+++ src/main/java/apt/auctionapi/config/MongoConfig.java
@@ -1,12 +1,25 @@
 package apt.auctionapi.config;
 
+import java.util.Arrays;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import apt.auctionapi.entity.StringToLocalDateConverter;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "apt.auctionapi.repository")
 @EnableMongoAuditing
 public class MongoConfig {
+
+    @Bean
+    public MongoCustomConversions customConversions() {
+        return new MongoCustomConversions(Arrays.asList(
+            new StringToLocalDateConverter() // String → LocalDate
+            // new LocalDateToStringConverter()   // LocalDate → String
+        ));
+    }
 }
---
+++ src/main/java/apt/auctionapi/entity/StringToLocalDateConverter.java
@@ -0,0 +1,24 @@
+package apt.auctionapi.entity;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+/**
+ * 📌 MongoDB String 날짜값 ("20240509") ↔ LocalDate 변환 컨버터
+ */
+@ReadingConverter
+@Component
+public class StringToLocalDateConverter implements Converter<String, LocalDate> {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.BASIC_ISO_DATE; // "yyyyMMdd"
+
+    @Override
+    public LocalDate convert(@NonNull String source) {
+        return LocalDate.parse(source, FORMATTER);
+    }
+}
