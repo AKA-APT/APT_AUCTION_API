86449e170820e0f0e84c5cf551b1a23ae1bab98e
강인화 <rkddlsghk1998@gmail.com>
2025-03-20 23:40:06 +0900

    fix: 메서드 삭제

--- src/main/java/apt/auctionapi/repository/InvestmentTagRepository.java
+++ src/main/java/apt/auctionapi/repository/InvestmentTagRepository.java
@@ -1,15 +1,11 @@
 package apt.auctionapi.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import apt.auctionapi.entity.Member;
 import apt.auctionapi.entity.Tag;
 
 public interface InvestmentTagRepository extends JpaRepository<Tag, Long> {
-
-    void saveAll(List<Tag> memberInvestmentTags);
 
     void deleteByMember(Member member);
 }
