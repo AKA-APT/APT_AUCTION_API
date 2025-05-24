a859a5d0e522dfe75bf5f31b08bc2754d39421d0
강인화 <rkddlsghk1998@gmail.com>
2025-05-25 07:36:06 +0900

    feat: 정보추가

--- src/main/java/apt/auctionapi/entity/AuctionDetail.java
+++ src/main/java/apt/auctionapi/entity/AuctionDetail.java
@@ -1,13 +1,14 @@
 package apt.auctionapi.entity;
 
 import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import jakarta.persistence.Id;
 import lombok.Getter;
 
 @Getter
@@ -21,14 +22,16 @@ public class AuctionDetail {
     private ObjectId auctionId;
 
     private int photoCount;
-
     private List<RightHolderGroup> rightHolders;
-
-    private LandInfo landInfo;
-
-    private String keyword;
-
     private List<OccupantInfo> occupantInfoList;
+    private List<AuctionObject> objectList;
+    private List<PublishedPrice> publishedPriceList;
+
+    @Field("landInfo")
+    private LandInfoDetail landInfoDetail;
+
+    private List<OccupancyInfo> occupancyInfoList;
+    private List<OccupancyRelationship> occupancyRelationshipList;
 
     @Getter
     public static class RightHolderGroup {
@@ -41,22 +44,6 @@ public class AuctionDetail {
         private String owner;
         private String debtor;
         private List<String> creditors;
-    }
-
-    @Getter
-    public static class LandInfo {
-        private List<PublishedPrice> publishedPriceList;
-    }
-
-    @Getter
-    public static class PublishedPrice {
-        private String pnu;
-        private int stdYear;
-        private double totalPublishedPrice;
-        private double publishedPrice;
-        private double sdPublishedPrice;
-        private double sggPublishedPrice;
-        private double emdPublishedPrice;
     }
 
     @Getter
@@ -80,5 +67,153 @@ public class AuctionDetail {
         private Double simpleDeposit;
         private Double simpleLeasePrice;
         private Double simpleExpectedDividends;
+    }
+
+    @Getter
+    public static class AuctionObject {
+        private String auctionId;
+        private String caseId;
+        private String caseSite;
+        private int itemNumber;
+        private int objectNumber;
+        private String objectType;
+        private String usage;
+        private String area;
+        private String objectAddress;
+        private String groundTotalArea;
+        private String buildingTotalArea;
+        private String otherBuildingTotalArea;
+        private String objectDetail;
+        private Integer isPartSell;
+    }
+
+    @Getter
+    public static class PublishedPrice {
+        private String pnu;
+        private int stdYear;
+        private double totalPublishedPrice;
+        private double publishedPrice;
+        private double sdPublishedPrice;
+        private double sggPublishedPrice;
+        private double emdPublishedPrice;
+    }
+
+    @Getter
+    public static class LandInfoDetail {
+        private String pnu;
+        private String bjdCode;
+        private BjdInfo bjdInfo;
+        private String addressBjd;
+        private String addressRoad;
+        private String bunji;
+        private String landCategoryName;
+        private String landPurposeName1;
+        private String landPurposeName2;
+        private String landUseName;
+        private String heightName;
+        private String landShapeName;
+        private String roadSideName;
+        private int years;
+        private double area;
+        private double TPUP;
+        private double PUP;
+        private int BD;
+        private int DBD;
+        private double grossAreaSum;
+        private int maxFloorArea;
+        private int maxBuildingRatio;
+        private BuildingInfo buildingInfo;
+    }
+
+    @Getter
+    public static class BjdInfo {
+        private String sd;
+        private String sgg;
+        private String emd;
+        private String dr;
+        private String bjdCode;
+    }
+
+    @Getter
+    public static class BuildingInfo {
+        private Object masterConstructionInfo;
+        private List<MasterBuildingInfo> masterBuildingInfoList;
+    }
+
+    @Getter
+    public static class MasterBuildingInfo {
+        private String buildingMasterId;
+        private String danji;
+        private String building;
+        private String pnu;
+        private String bjdCode;
+        private LocalDate constructDate;
+        private double grossArea;
+        private int faEstimateToGross;
+        private double floorArea;
+        private double constructArea;
+        private double buildingRatio;
+        private String mainPurposeName;
+        private String mainPurposeIndex;
+        private String etcPurpose;
+        private String mainOrPartDivName;
+        private int mechParking;
+        private int selfParking;
+        private int groundFloorCnt;
+        private int undergroundFloorCnt;
+        private String roofName;
+        private String mainStructureMaterial;
+        private int elevatorCnt;
+        private int emergencyElevatorCnt;
+        private List<FloorInfo> floorInfoList;
+    }
+
+    @Getter
+    public static class FloorInfo {
+        private String buildingMasterId;
+        private String floorType;
+        private int floor;
+        private double area;
+        private String mainOrPartDivName;
+        private String mainPurposeName;
+        private String etcPurposeName;
+        private String mainStructureMaterial;
+        private String etcStructureMaterial;
+    }
+
+    @Getter
+    public static class OccupancyInfo {
+        private String auctionId;
+        private String caseId;
+        private String caseSite;
+        private int itemNumber;
+        private int objectNumber;
+        private int occupantNumber;
+        private String objectAddress;
+        private String occupant;
+        private String occupantType;
+        private String occupancyPart;
+        private String occupancyUsage;
+        private String occupancyPeriod;
+        private Double deposit;
+        private Double leasePrice;
+        private String leasePriceType;
+        private OffsetDateTime registrationDt;
+        private OffsetDateTime fixedDt;
+        private Double simpleDeposit;
+        private Double simpleLeasePrice;
+    }
+
+    @Getter
+    public static class OccupancyRelationship {
+        private String auctionId;
+        private String caseId;
+        private String caseSite;
+        private int itemNumber;
+        private int objectNumber;
+        private String objectAddress;
+        private String occupancyRelationship;
+        private String occupancyRelationshipInfo;
+        private String status;
     }
 }
